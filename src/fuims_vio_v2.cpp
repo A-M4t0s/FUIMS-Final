515a068d3b4b1f95df10163f86cd15e4a8027f5b
A-M4t0s <1211177@isep.ipp.pt>
2025-12-16 14:06:14 +0000

    VIO v2 - Feature Matching

--- src/fuims_vio_v2.cpp
+++ src/fuims_vio_v2.cpp
@@ -39,7 +39,7 @@
 #include <opencv2/viz.hpp>
 
 #define BAG_PATH "/home/tony/catkin_ws/bags/bags_fuims/agucadoura.bag"
-#define CAMERA_TOPIC "/dji_m350/cameras/main/raw"
+#define CAMERA_TOPIC "/dji_m350/cameras/main/compressed"
 #define QUATERNION_TOPIC "/dji_m350/quaternion"
 #define VELOCITY_TOPIC "/dji_m350/velocity"
 #define GPS_TOPIC "/dji_m350/gps"
@@ -54,10 +54,10 @@ constexpr double b = a * (1 - f);
 constexpr double e2 = 1 - (b * b) / (a * a);
 
 /*
-* ================================================================================================================
-* Data Structure Definitions
-* ================================================================================================================
-*/
+ * ================================================================================================================
+ * Data Structure Definitions
+ * ================================================================================================================
+ */
 struct ENU
 {
     double x, y, z;
@@ -78,10 +78,10 @@ struct Keyframe
 };
 
 /*
-* ================================================================================================================
-* GPS <-> ENU Conversions
-* ================================================================================================================
-*/
+ * ================================================================================================================
+ * GPS <-> ENU Conversions
+ * ================================================================================================================
+ */
 void geodeticToECEF(double lat, double lon, double alt, double &X, double &Y, double &Z)
 {
     double sinLat = sin(lat), cosLat = cos(lat);
@@ -116,21 +116,325 @@ ENU ecefToENU(double X, double Y, double Z,
 }
 
 /*
-* ================================================================================================================
-* Class: vioManager
-* ================================================================================================================
-*/
-class vioManager{
+ * ================================================================================================================
+ * Class: vioManager
+ * ================================================================================================================
+ */
+class vioManager
+{
 public:
+    vioManager()
+    {
+        // =========================================================
+        // Starting Publishers
+        // =========================================================
+        debugPub = nh.advertise<sensor_msgs::Image>("vio/debug_image", 1);
+
+        // =========================================================
+        // Opening bag
+        // =========================================================
+        ROS_INFO("Opening Bag...");
+        try
+        {
+            bag.open(BAG_PATH, rosbag::bagmode::Read);
+            ROS_INFO("Opened bag: %s", BAG_PATH);
+        }
+        catch (rosbag::BagException &e)
+        {
+            ROS_ERROR("Failed to open bag: %s. Error: %s", BAG_PATH, e.what());
+            return;
+        }
+
+        // =========================================================
+        // Loading Messages from ROSBAG
+        // =========================================================
+        ROS_INFO("Loading messages from ROSBAG...");
+        //  -> Camera Images
+        rosbag::View cam_view(bag, rosbag::TopicQuery(CAMERA_TOPIC));
+
+        // -> Quaternion Messages
+        rosbag::View quat_view(bag, rosbag::TopicQuery(QUATERNION_TOPIC));
+        for (const rosbag::MessageInstance &m : quat_view)
+        {
+            auto msg = m.instantiate<geometry_msgs::QuaternionStamped>();
+            if (msg)
+                quatMsgs.push_back(msg);
+        }
+        ROS_INFO("Loaded %zu quaternion messages", quatMsgs.size());
+
+        // -> Velocity Messages
+        rosbag::View vel_view(bag, rosbag::TopicQuery(VELOCITY_TOPIC));
+        for (const rosbag::MessageInstance &m : vel_view)
+        {
+            auto msg = m.instantiate<geometry_msgs::Vector3Stamped>();
+            if (msg)
+                velMsgs.push_back(msg);
+        }
+        ROS_INFO("Loaded %zu velocity messages", velMsgs.size());
+
+        // -> GPS Messages
+        rosbag::View gps_view(bag, rosbag::TopicQuery(GPS_TOPIC));
+        for (const rosbag::MessageInstance &m : gps_view)
+        {
+            auto msg = m.instantiate<sensor_msgs::NavSatFix>();
+            if (msg)
+                gpsMsgs.push_back(msg);
+        }
+        ROS_INFO("Loaded %zu GPS messages", gpsMsgs.size());
+
+        // =========================================================
+        // Initializing ORB - 500 Features
+        // =========================================================
+        orb = cv::ORB::create(750);
+
+        // =========================================================
+        // Initializing GTSAM
+        // =========================================================
+        gtsam::Pose3 prior;
+        noise = gtsam::noiseModel::Diagonal::Sigmas(
+            (gtsam::Vector(6) << 0.1, 0.1, 0.1, 0.1, 0.1, 0.1).finished());
+        graph.add(gtsam::PriorFactor<gtsam::Pose3>(gtsam::Symbol('x', 0), prior, noise));
+        values.insert(gtsam::Symbol('x', 0), prior);
+
+        bool firstFrame = true;
+        int frameIdx = 0;
+        // =========================================================
+        // Main Loop - Processing each frame
+        // =========================================================
+        for (const rosbag::MessageInstance &m : cam_view)
+        {
+            ROS_INFO("Processing frame %d", frameIdx++);
+
+            auto imgMsg = m.instantiate<sensor_msgs::CompressedImage>();
+            if (!imgMsg)
+                continue;
+
+            // Undistorting Image
+            undistortImage(imgMsg);
+
+            // If first frame, just detect features
+            if (firstFrame)
+            {
+                featureDetection(currUndistortedGrey);
+                prevPoints = currPoints;
+                prevUndistortedGrey = currUndistortedGrey.clone();
+                firstFrame = false;
+                continue;
+            }
+
+            // Verifiying we have previous points to track
+            if (prevPoints.pts.size() < 25) // At least 25 points to track
+            {
+                ROS_WARN("No previous points to track, detecting new features");
+                featureDetection(currUndistortedGrey);
+                prevPoints = currPoints;
+                prevUndistortedGrey = currUndistortedGrey.clone();
+                continue;
+            }
+
+            // Feature Tracking (KLT)
+            std::vector<cv::Point2f> trackedPoints;
+            std::vector<uchar> status;
+            std::vector<float> error;
+
+            cv::calcOpticalFlowPyrLK(
+                prevUndistortedGrey,
+                currUndistortedGrey,
+                prevPoints.pts,
+                trackedPoints,
+                status,
+                error,
+                cv::Size(21, 21),
+                2);
+
+            // Build filtered correspondences
+            Points currFiltered;
+            std::vector<cv::Point2f> prevValid, currValid;
+
+            auto inImage = [&](const cv::Point2f &p, const cv::Mat &img)
+            {
+                return p.x >= 8 && p.y >= 8 &&
+                       p.x < img.cols - 8 &&
+                       p.y < img.rows - 8;
+            };
+
+            for (size_t k = 0; k < status.size(); k++)
+            {
+                if (!status[k])
+                    continue;
+
+                const cv::Point2f &p_prev = prevPoints.pts[k];
+                const cv::Point2f &p_curr = trackedPoints[k];
+
+                if (!inImage(p_prev, prevUndistortedGrey) ||
+                    !inImage(p_curr, currUndistortedGrey))
+                    continue;
+
+                if (cv::norm(p_curr - p_prev) > 60.0f)
+                    continue;
+
+                prevValid.push_back(p_prev);
+                currValid.push_back(p_curr);
+
+                currFiltered.pts.push_back(p_curr);
+                currFiltered.ids.push_back(prevPoints.ids[k]);
+            }
+
+            currPoints = currFiltered;
+
+            // If number of tracked points below threshold, detect new features
+            if (currPoints.pts.size() < KF_FEATURE_THRESHOLD)
+            {
+                Points trackedBackup = currPoints;
+                featureDetection(currUndistortedGrey);
+
+                currPoints.pts.insert(currPoints.pts.end(),
+                                      trackedBackup.pts.begin(),
+                                      trackedBackup.pts.end());
+                currPoints.ids.insert(currPoints.ids.end(),
+                                      trackedBackup.ids.begin(),
+                                      trackedBackup.ids.end());
+            }
+
+            // Debug Image Publishing
+            cv::Mat prevBgr, currBgr;
+            cv::cvtColor(prevUndistortedGrey, prevBgr, cv::COLOR_GRAY2BGR);
+            cv::cvtColor(currUndistortedGrey, currBgr, cv::COLOR_GRAY2BGR);
+
+            cv::Mat debugImg;
+            cv::hconcat(prevBgr, currBgr, debugImg);
+
+            int offsetX = prevBgr.cols;
+
+            for (size_t k = 0; k < prevValid.size(); k++)
+            {
+                cv::Point2f p1 = prevValid[k];
+                cv::Point2f p2 = currValid[k] + cv::Point2f((float)offsetX, 0.0f);
+
+                cv::circle(debugImg, p1, 3, cv::Scalar(0, 255, 0), -1);
+                cv::circle(debugImg, p2, 3, cv::Scalar(0, 0, 255), -1);
+                cv::line(debugImg, p1, p2, cv::Scalar(255, 0, 0), 1);
+            }
+
+            sensor_msgs::ImagePtr debugMsg =
+                cv_bridge::CvImage(std_msgs::Header(), "bgr8", debugImg).toImageMsg();
+
+            debugMsg->header.stamp = m.getTime();
+            debugPub.publish(debugMsg);
+
+            // Preparing for next iteration
+            prevUndistortedGrey = currUndistortedGrey.clone();
+            prevPoints = currPoints;
+        }
+    }
 
 private:
+    /*
+     * ================================================================================================================
+     * Variables
+     * ================================================================================================================
+     */
+    // ROS Variables
+    ros::NodeHandle nh;
+    ros::Publisher debugPub, pathPub, cloudPub;
+    rosbag::Bag bag;
+
+    // ROSBAG Messages Vectors
+    std::vector<sensor_msgs::CompressedImageConstPtr> camMsgs;
+    std::vector<geometry_msgs::QuaternionStampedConstPtr> quatMsgs;
+    std::vector<geometry_msgs::Vector3StampedConstPtr> velMsgs;
+    std::vector<sensor_msgs::NavSatFixConstPtr> gpsMsgs;
+
+    // Camera Parameters
+    const cv::Mat K = (cv::Mat_<double>(3, 3) << 1372.76165, 0.0, 960.45289,
+                       0.0, 1372.14817, 515.00383,
+                       0.0, 0.0, 1.0);
+    const cv::Mat distCoeffs = (cv::Mat_<double>(1, 5) << 0.048300, -0.044905, -0.003731, -0.001349, 0);
+
+    // ORB Detector
+    cv::Ptr<cv::ORB> orb;
+
+    // Images
+    cv::Mat currUndistortedGrey, currUndistortedRGB;
+    cv::Mat prevUndistortedGrey, prevUndistortedRGB;
+
+    // Feature Points
+    Points currPoints, prevPoints;
+    int nextFeatureID = 0;
+
+    // GTSAM Variables
+    gtsam::NonlinearFactorGraph graph;
+    gtsam::noiseModel::Diagonal::shared_ptr noise;
+    gtsam::Values values;
+
+    /*
+     * ================================================================================================================
+     * Methods
+     * ================================================================================================================
+     */
+    // Method for Undistorting an Image
+    void undistortImage(sensor_msgs::CompressedImageConstPtr msg)
+    {
+        cv::Mat raw = cv::imdecode(msg->data, cv::IMREAD_COLOR);
+        cv::undistort(raw, currUndistortedRGB, K, distCoeffs);
+        cv::cvtColor(currUndistortedRGB, currUndistortedGrey, cv::COLOR_BGR2GRAY);
+    }
+
+    // Method for Feature Detection using ORB
+    void featureDetection(const cv::Mat &img)
+    {
+        currPoints.pts.clear();
+        currPoints.ids.clear();
+
+        // Variable Declarations
+        std::vector<cv::KeyPoint> kp;
+
+        // ORB Keypoint Detection
+        orb->detect(img, kp);
+
+        // Ordering Keypoints by Response
+        std::vector<int> idx(kp.size());
+        std::iota(idx.begin(), idx.end(), 0);
+        std::sort(idx.begin(), idx.end(),
+                  [&](int a, int b)
+                  { return kp[a].response > kp[b].response; });
+
+        // Defining a Mask in order to set a minimum distance between keypoints
+        cv::Mat mask(img.size(), CV_8UC1, cv::Scalar(255));
+        int radius = 21;
+
+        // Selecting the ORB_N_BEST Keypoints
+        int kept = 0;
+        for (int id : idx)
+        {
+            cv::Point2f p = kp[id].pt;
+            int x = cvRound(p.x);
+            int y = cvRound(p.y);
+
+            if (x < 0 || x >= img.cols || y < 0 || y >= img.rows)
+                continue;
+
+            if (mask.at<uint8_t>(y, x) == 255)
+            {
+                currPoints.pts.push_back(p);
+                currPoints.ids.push_back(nextFeatureID++);
+                cv::circle(mask, cv::Point(x, y), radius, 0, -1);
+
+                kept++;
+                if (kept == ORB_N_BEST)
+                    break;
+            }
+        }
+    }
+
+    // Method for Visualizing Feature Matching Debug Images
 };
 
 /*
-* ================================================================================================================
-* Main
-* ================================================================================================================
-*/
+ * ================================================================================================================
+ * Main
+ * ================================================================================================================
+ */
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "fuims_vio");
--- src/rosbag_correction.cpp
+++ src/rosbag_correction.cpp
@@ -2,19 +2,14 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 
-#include <sensor_msgs/image_encodings.h>
-#include <sensor_msgs/Image.h>
 #include <sensor_msgs/CompressedImage.h>
 #include <sensor_msgs/NavSatFix.h>
-#include <sensor_msgs/point_cloud2_iterator.h>
-#include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/QuaternionStamped.h>
 #include <geometry_msgs/Vector3Stamped.h>
 
-#include <opencv2/opencv.hpp>
-
 #define INPUT_BAG_PATH "/home/tony/catkin_ws/bags/bags_Agucadoura/bom/2025-07-03-13-03-16.bag"
 #define OUTPUT_BAG_PATH "/home/tony/catkin_ws/bags/bags_fuims/agucadoura.bag"
+
 #define CAMERA_TOPIC "/dji_m350/cameras/main/compressed"
 #define QUATERNION_TOPIC "/dji_m350/quaternion"
 #define VELOCITY_TOPIC "/dji_m350/velocity"
@@ -25,59 +20,37 @@ class rosbagUpdater
 public:
     rosbagUpdater()
     {
-        // Opening Input and Output Bags
         input.open(INPUT_BAG_PATH, rosbag::bagmode::Read);
         output.open(OUTPUT_BAG_PATH, rosbag::bagmode::Write);
 
-        // Topics list
         std::vector<std::string> topics = {
             CAMERA_TOPIC,
             QUATERNION_TOPIC,
             VELOCITY_TOPIC,
             GPS_TOPIC};
-        rosbag::View view(input, rosbag::TopicQuery(topics)); // View of all the needed topics
 
-        size_t frame_id = 0;
+        rosbag::View view(input, rosbag::TopicQuery(topics));
 
-        // Input bag processing
         for (const rosbag::MessageInstance &m : view)
         {
-            // Get current time
             ros::Time t = m.getTime();
 
-            // Camera Message Processing
+            // CAMERA (CompressedImage)
             if (m.getTopic() == CAMERA_TOPIC)
             {
-                sensor_msgs::CompressedImageConstPtr comp = m.instantiate<sensor_msgs::CompressedImage>();
-                if (!comp)
-                    continue;
+                sensor_msgs::CompressedImageConstPtr img =
+                    m.instantiate<sensor_msgs::CompressedImage>();
 
-                // Converting CompressedImage to Image
-                cv::Mat img = cv::imdecode(cv::Mat(comp->data), cv::IMREAD_COLOR);
-                if (img.empty())
-                    continue;
+                if (img)
+                    output.write(CAMERA_TOPIC, t, *img);
 
-                sensor_msgs::Image raw;
-                raw.header.stamp = t;
-                raw.header.frame_id = comp->header.frame_id;
-                raw.height = img.rows;
-                raw.width = img.cols;
-                raw.encoding = sensor_msgs::image_encodings::BGR8;
-                raw.step = img.cols * 3;
-                raw.data.assign(img.data, img.data + img.total() * img.elemSize());
-
-                output.write("/dji_m350/cameras/main/raw", t, raw);
-
-                frame_id++;
                 continue;
             }
 
             // QUATERNION
             if (m.getTopic() == QUATERNION_TOPIC)
             {
-                geometry_msgs::QuaternionStampedConstPtr q =
-                    m.instantiate<geometry_msgs::QuaternionStamped>();
-
+                auto q = m.instantiate<geometry_msgs::QuaternionStamped>();
                 if (q)
                     output.write(QUATERNION_TOPIC, t, *q);
 
@@ -87,9 +60,7 @@ public:
             // VELOCITY
             if (m.getTopic() == VELOCITY_TOPIC)
             {
-                geometry_msgs::Vector3StampedConstPtr v =
-                    m.instantiate<geometry_msgs::Vector3Stamped>();
-
+                auto v = m.instantiate<geometry_msgs::Vector3Stamped>();
                 if (v)
                     output.write(VELOCITY_TOPIC, t, *v);
 
@@ -99,9 +70,7 @@ public:
             // GPS
             if (m.getTopic() == GPS_TOPIC)
             {
-                sensor_msgs::NavSatFixConstPtr gps =
-                    m.instantiate<sensor_msgs::NavSatFix>();
-
+                auto gps = m.instantiate<sensor_msgs::NavSatFix>();
                 if (gps)
                     output.write(GPS_TOPIC, t, *gps);
 
@@ -114,11 +83,9 @@ public:
     }
 
 private:
-    // ROSBAG Variables
     rosbag::Bag input, output;
 };
 
-// Main
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rosbag_updater");
